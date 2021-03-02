cfb944e0e2c28e0414c90ba658b6a6db45170b1a
Jake Steuver <jakesteuver@gmail.com>
2021-03-02 08:52:45 -0500

    Add basic player sprite and movement

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private int speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.velocity = new Vector2(0, speed * -1);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector2(0, speed);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+    }
+}
