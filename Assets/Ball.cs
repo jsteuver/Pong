070258de859a091a2272f1c72e50c035f33e05f3
Jake Steuver <jakesteuver@gmail.com>
2021-03-02 16:44:36 -0500

    Add ball with random start angle

---
+++ Assets/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float speed = 15;
+    private float xVector;
+    private float yVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        xVector = Random.Range(-15.0f, 15.0f);
+        yVector = Mathf.Sqrt(speed * speed - xVector * xVector) * (Random.Range(0, 2) * 2 - 1);
+
+        rb.velocity = new Vector2(xVector, yVector);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
